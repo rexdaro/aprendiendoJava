d1def96dc1e3424c4c833d204e6a1b4a9431f61b
manuel rivas linux <rexdaro@gmail.com>
2025-11-18 13:11:10 -0300

    first

---
+++ PrimerProyectoJava/src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int edad = 27;
+        double precio = 15.5;
+        String nombre = "Manu";
+
+        boolean mayorDeEdad = edad >= 18;
+
+
+        System.out.println("Edad: " + edad);
+        System.out.println("Precio: " + precio);
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Es mayor de edad: " + mayorDeEdad);
+
+        
+
+    }
+}
