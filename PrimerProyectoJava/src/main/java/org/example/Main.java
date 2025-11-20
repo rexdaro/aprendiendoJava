28bfe824b353dd7de044c2ab8405a2de053dcdb8
manuel rivas linux <rexdaro@gmail.com>
2025-11-20 13:39:25 -0300

    dia 2 y 3, siwch and bucles

--- PrimerProyectoJava/src/main/java/org/example/Main.java
+++ PrimerProyectoJava/src/main/java/org/example/Main.java
@@ -5,19 +5,36 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        int edad = 27;
-        double precio = 15.5;
-        String nombre = "Manu";
+       int diaDeLaSemana = 7;
 
-        boolean mayorDeEdad = edad >= 18;
+       switch (diaDeLaSemana) {
+           case 1:
+               System.out.println("Lunes");
+               break;
+           case 2:
+               System.out.println("Martes");
+               break;
+           case 3:
+               System.out.println("Miércoles");
+               break;
+           case 4:
+               System.out.println("Jueves");
+               break;
+           case 5:
+               System.out.println("Viernes");
+               break;
+           case 6:
+               System.out.println("Sábado");
+               break;
+           case 7:
+               System.out.println("Domingo");
+               break;
+           default:
+               System.out.println("Día inválido");
+               break;
 
 
-        System.out.println("Edad: " + edad);
-        System.out.println("Precio: " + precio);
-        System.out.println("Nombre: " + nombre);
-        System.out.println("Es mayor de edad: " + mayorDeEdad);
 
-        
-
+       }
     }
 }
