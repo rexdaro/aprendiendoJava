8b58a65b1388f374ae867f920d3b9b2385172c5b
manuel rivas linux <rexdaro@gmail.com>
2025-11-21 18:26:19 -0300

    ejercicio constantes

--- PrimerProyectoJava/src/main/java/org/example/Main.java
+++ PrimerProyectoJava/src/main/java/org/example/Main.java
@@ -4,9 +4,29 @@ import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    static final int CANT_PASADAS = 3;
+
     public static void main(String[] args) {
 
-        System.out.println("Descartes dijo: \"Pienso, luego existo\".");
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.print("ingrese el ancho del terreno: ");
+        int ancho = Integer.parseInt(input.nextLine());
+        System.out.print("ingrese el largo del terreno: ");
+        int largo = Integer.parseInt(input.nextLine());
+        System.out.print("ingrese el precio del metro cuadrado del terreno: ");
+        int precioM2 = Integer.parseInt(input.nextLine());
+
+
+
+        int areaDeTerreno = ancho * largo;
+        int cantidadDeAlambre = ((ancho * 2) + (largo * 2)) * CANT_PASADAS;
+        int precioTerreno = precioM2 * areaDeTerreno;
+
+        System.out.println(precioTerreno + "usd");
+        System.out.println(cantidadDeAlambre + "m");
 
     }
 }
