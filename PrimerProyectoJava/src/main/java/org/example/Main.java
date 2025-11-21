26711b2cc5e7683f57d33c1ea6154a93b701a22b
manuel rivas linux <rexdaro@gmail.com>
2025-11-21 07:02:18 -0300

    commit dia 21/11/2025

--- PrimerProyectoJava/src/main/java/org/example/Main.java
+++ PrimerProyectoJava/src/main/java/org/example/Main.java
@@ -1,40 +1,12 @@
 package org.example;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-       int diaDeLaSemana = 7;
+        System.out.println("Descartes dijo: \"Pienso, luego existo\".");
 
-       switch (diaDeLaSemana) {
-           case 1:
-               System.out.println("Lunes");
-               break;
-           case 2:
-               System.out.println("Martes");
-               break;
-           case 3:
-               System.out.println("Miércoles");
-               break;
-           case 4:
-               System.out.println("Jueves");
-               break;
-           case 5:
-               System.out.println("Viernes");
-               break;
-           case 6:
-               System.out.println("Sábado");
-               break;
-           case 7:
-               System.out.println("Domingo");
-               break;
-           default:
-               System.out.println("Día inválido");
-               break;
-
-
-
-       }
     }
 }
