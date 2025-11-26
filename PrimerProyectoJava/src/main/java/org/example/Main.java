5bfa87e9fcc6687974ddef0a4a60a7b53180365a
manuel rivas linux <rexdaro@gmail.com>
2025-11-26 19:02:49 -0300

    26 11 2026

--- PrimerProyectoJava/src/main/java/org/example/Main.java
+++ PrimerProyectoJava/src/main/java/org/example/Main.java
@@ -5,18 +5,11 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-
-    static final String NOMBRE = "Manuel";
-    static  final String APELLIDO = "Rivas";
-
     public static void main(String[] args) {
 
-        saludar(NOMBRE, APELLIDO);
+
+        System.out.println("holamundo!");
+
+
     }
-
-    public static void saludar(String nombre1, String nombre2){
-
-        System.out.println("Hola " + nombre1 + " " + nombre2);
-
-    };
 }
