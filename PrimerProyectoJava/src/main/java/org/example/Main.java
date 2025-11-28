331a80a9dcd2aa109a9ba617807d85a5b45fc90e
manuel rivas linux <rexdaro@gmail.com>
2025-11-28 12:55:48 -0300

    dia 7

--- PrimerProyectoJava/src/main/java/org/example/Main.java
+++ PrimerProyectoJava/src/main/java/org/example/Main.java
@@ -6,20 +6,30 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        int[] numeros = {12, 5, 7, 20, 9, 18, 3};
+        int multiplosDe3 = 0;
+        int numerosMayoresA10 = 0;
+        int sumaTotal = 0;
+        double promedio;
 
-        Scanner input = new Scanner(System.in);
+        for (int i = 0; i < numeros.length; i++) {
+            sumaTotal += numeros[i];
 
-        int estrellas;
+            if (numeros[i] > 10){
+                numerosMayoresA10++;
+            }
 
-        do {
-            System.out.print("Ingrese la cantidad de la base de estrellas que contendra el arbol de navidad: ");
-            estrellas = Integer.parseInt(input.nextLine());
-        } while (estrellas % 2 == 0 || estrellas < 3);
+            if (numeros[i] > 0 && ((numeros[i] % 3) == 0)){
+                multiplosDe3++;
+            }
+        }
 
+        promedio = (double) sumaTotal / numeros.length;
 
-
-
-
-
+        System.out.println("el promedio es: " + promedio);
+        System.out.println("hay: " + numerosMayoresA10 + " numeros mayores a 10.");
+        System.out.println("hay: " + multiplosDe3 + " numeros multiplos de 3.");
     }
+
+
 }
