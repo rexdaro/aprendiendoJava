0fd898934fd6fdbdd2fdde572eeb831491382223
manuel rivas linux <rexdaro@gmail.com>
2025-11-29 21:24:29 -0300

    dia 1 programacion funcional

--- PrimerProyectoJava/src/main/java/org/example/Main.java
+++ PrimerProyectoJava/src/main/java/org/example/Main.java
@@ -1,4 +1,5 @@
 package org.example;
+import java.sql.Array;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -6,30 +7,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int[] numeros = {12, 5, 7, 20, 9, 18, 3};
-        int multiplosDe3 = 0;
-        int numerosMayoresA10 = 0;
-        int sumaTotal = 0;
-        double promedio;
 
-        for (int i = 0; i < numeros.length; i++) {
-            sumaTotal += numeros[i];
-
-            if (numeros[i] > 10){
-                numerosMayoresA10++;
-            }
-
-            if (numeros[i] > 0 && ((numeros[i] % 3) == 0)){
-                multiplosDe3++;
-            }
-        }
-
-        promedio = (double) sumaTotal / numeros.length;
-
-        System.out.println("el promedio es: " + promedio);
-        System.out.println("hay: " + numerosMayoresA10 + " numeros mayores a 10.");
-        System.out.println("hay: " + multiplosDe3 + " numeros multiplos de 3.");
+     Scanner input = new Scanner(System.in);
     }
-
 
 }
