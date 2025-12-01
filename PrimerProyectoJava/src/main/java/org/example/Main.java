87639f0fa5a865e3f81f14abf1722dd39a170c28
manuel rivas linux <rexdaro@gmail.com>
2025-12-01 10:53:27 -0300

    programa de numero perfecto

--- PrimerProyectoJava/src/main/java/org/example/Main.java
+++ PrimerProyectoJava/src/main/java/org/example/Main.java
@@ -8,7 +8,47 @@ public class Main {
 
     public static void main(String[] args) {
 
-     Scanner input = new Scanner(System.in);
+        imprimir(esPerfecto());
+
     }
+
+    static void imprimir (boolean x) {
+        if (x){
+            System.out.println("su numero si es un numero perfecto");
+        } else {
+            System.out.println("su numero no es un numero perfecto");
+        }
+
+    }
+
+    static boolean esPerfecto () {
+
+        int num = pedirNumero();
+        return  num == calcularPerfecto(num);
+    }
+
+    static int pedirNumero () {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Escriba un numero: ");
+
+        return Integer.parseInt(input.nextLine());
+    }
+
+    static int calcularPerfecto (int num) {
+
+        int sumatoriaDeDivisores = 0;
+
+        for (int i = 1; i < num; i++) {
+            if (num % i == 0){
+                sumatoriaDeDivisores += i;
+            }
+        }
+
+
+        return sumatoriaDeDivisores;
+    }
+
 
 }
