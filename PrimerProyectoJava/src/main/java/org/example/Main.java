f44939dc09ca183ae21806c54a595d7230b9afe9
manuel rivas linux <rexdaro@gmail.com>
2025-12-06 12:10:06 -0300

    dia 6 diciembre

--- PrimerProyectoJava/src/main/java/org/example/Main.java
+++ PrimerProyectoJava/src/main/java/org/example/Main.java
@@ -7,41 +7,23 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
+    public static double DESCUENTO_ESPECIAL  =  0.10;
+
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-
         System.out.print("Ingrese el precio del producto: ");
         double precio = Integer.parseInt(input.nextLine());
-        System.out.print("Ingrese el peso del paquete en kg: ");
-        double peso = Integer.parseInt(input.nextLine());
-        System.out.print("Ingrese la zona de envío (Nacional/Internacional): ");
-        String zona = input.nextLine();
 
+        double descuentoAplicado = calcularDescuentoEspecial(precio);
 
-
-
-        System.out.println("El costo de envío es: " + calcularCostoEnvio(peso, zona));
-        System.out.println("El costo de envío es: " + calcularTotalCompra(peso, precio, zona));
-
+        System.out.println("El descuento especial aplicado es: " + descuentoAplicado);
+        System.out.println("El precio final con descuento es: " + (precio - descuentoAplicado));
 
     }
 
-    static double calcularCostoEnvio(double peso, String zona){
-        if (zona.equals("Nacional")) {
-            return 5 * peso;
-        } else {
-            return 10 * peso;
-        }
-    };
 
-    static double calcularTotalCompra(double peso, double precio, String zona) {
-
-        double envio = calcularCostoEnvio(peso, zona);
-
-
-        return precio + envio;
-    };
-
-
+    static double calcularDescuentoEspecial(double  precio){
+        return precio * DESCUENTO_ESPECIAL;
+    }
 }
