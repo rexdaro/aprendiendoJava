a0b3c2840f1c30d94bb5df1dc53192205de95236
manuel rivas linux <rexdaro@gmail.com>
2025-12-09 09:57:03 -0300

    dia 9 12

--- PrimerProyectoJava/src/main/java/org/example/Main.java
+++ PrimerProyectoJava/src/main/java/org/example/Main.java
@@ -7,23 +7,13 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    public static double DESCUENTO_ESPECIAL  =  0.10;
+
 
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
-        System.out.print("Ingrese el precio del producto: ");
-        double precio = Integer.parseInt(input.nextLine());
-
-        double descuentoAplicado = calcularDescuentoEspecial(precio);
-
-        System.out.println("El descuento especial aplicado es: " + descuentoAplicado);
-        System.out.println("El precio final con descuento es: " + (precio - descuentoAplicado));
 
     }
 
 
-    static double calcularDescuentoEspecial(double  precio){
-        return precio * DESCUENTO_ESPECIAL;
-    }
+
 }
